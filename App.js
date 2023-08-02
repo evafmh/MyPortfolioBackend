38527d853aca2bc6cab4699ad3d28ad8a61b1948
Eva F <eva.famechon@outlook.com>
2023-08-02 21:50:16 +0200

    Modify again again CORS

--- App.js
+++ App.js
@@ -15,19 +15,19 @@ app.use(cors({
 // Traitement des requêtes et réponses
 app.use(express.json())
 
-// Éviter les problèmes de CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-    )
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-    )
-    next()
-})
+// // Éviter les problèmes de CORS
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+//     )
+//     res.setHeader(
+//         'Access-Control-Allow-Methods',
+//         'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+//     )
+//     next()
+// })
 
 // Route pour envoyer l'e-mail
 app.use('/api', mailRouter)
